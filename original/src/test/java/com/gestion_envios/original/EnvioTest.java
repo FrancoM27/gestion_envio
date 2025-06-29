1b9a8ad4389e9d6ed34a69ac00dce45aa80a5795
FrancoM27 <fr.millar@duocuc.cl>
2025-06-29 03:32:44 -0400

    agregar tests

---
+++ original/src/test/java/com/gestion_envios/original/EnvioTest.java
@@ -0,0 +1,104 @@
+package com.gestion_envios.original;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.gestion_envios.original.Model.Envio;
+import com.gestion_envios.original.Model.Entity.EnvioEntity;
+import com.gestion_envios.original.Repository.EnvioRepository;
+import com.gestion_envios.original.Service.EnvioService;
+
+public class EnvioTest {
+    
+    @Mock
+    private EnvioRepository envioRepository;
+
+    @InjectMocks
+    private EnvioService envioService;
+
+    private Envio envio;
+    private EnvioEntity envioEntity;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        envio = new Envio(1, "valparaiso", "santiago", "enviado", "01/10/2025", "02/10/2025");
+        envioEntity = new EnvioEntity();
+        envioEntity.setIdEnvio(1);
+        envioEntity.setOrigen("valparaiso");
+        envioEntity.setDestino("santiago"); 
+        envioEntity.setEstado("enviado");
+        envioEntity.setFechaEnvio("01/10/2025");
+        envioEntity.setFechaEntrega("02/10/2025");
+    }
+
+    @Test
+    public void testCrearEnvio() {
+        // Simular el comportamiento del repositorio
+        when(envioRepository.existsByIdEnvio(envio.getIdEnvio())).thenReturn(false);
+        when(envioRepository.save(any(EnvioEntity.class))).thenReturn(envioEntity);
+
+        String result = envioService.crearEnvio(envio);
+        assertEquals("Envio creado con exito", result); 
+    }    
+    
+    @Test
+    public void testActualizarEnvio() {
+        // Simular el comportamiento del repositorio
+        when(envioRepository.findByIdEnvio(1)).thenReturn(envioEntity);
+        when(envioRepository.save(any(EnvioEntity.class))).thenReturn(envioEntity);
+
+        Envio nuevo = new Envio(1, "viña", "valparaiso", "entregado", "05/10/2025", "06/10/2025");
+        String result = envioService.actualizarEnvio(nuevo.getIdEnvio());
+        
+        assertEquals("Envio actualizado con exito", result); 
+    }
+
+    @Test
+    public void testObtenerEnvio() {
+        
+        when(envioRepository.findByIdEnvio(1)).thenReturn(envioEntity);
+        Envio result = envioService.obtenerEnvio(1);
+        assertNotNull(result);
+        assertEquals(envio.getIdEnvio(), result.getIdEnvio());           
+
+    }
+
+    @Test
+    public void testObtenerEnvioNoExistente() {
+        when(envioRepository.findByIdEnvio(2)).thenReturn(null);
+        Envio result = envioService.obtenerEnvio(2);
+        assertNull(result);
+    }
+
+    @Test
+    public void testEliminarEnvio(){
+        when(envioRepository.existsByIdEnvio(1)).thenReturn(true);
+        doNothing().when(envioRepository).deleteByIdEnvio(1);
+        String result = envioService.eliminarEnvio(1);
+
+        assertEquals("Envio eliminado con exito", result); 
+
+    } 
+
+    @Test
+    public void testCrearEnvioExistente() {
+        when(envioRepository.existsByIdEnvio(envio.getIdEnvio())).thenReturn(true);
+
+        String result = envioService.crearEnvio(envio);
+        assertEquals("El envio ya existe", result);
+
+    }
+}
